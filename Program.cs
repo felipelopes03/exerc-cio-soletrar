2da24ee1b413804977baeb5bf683606341b8007c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-05 22:26:28 -0300

    soletrandinho

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("digite uma palavra para soletrar");
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("\nsoletrando fica..\n");
+
+Console.WriteLine(string.Join("-",texto.ToUpper().ToArray()));
+
